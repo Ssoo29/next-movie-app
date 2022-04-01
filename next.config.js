3104d8c692bd4ba939209689e1b236e83b6741aa
김성수 <fereverv@openbase.co.kr>
2022-04-01 10:29:44 +0900

    Detail 컴포넌트 수정

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
       {
         source: "/api/movies",
-        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}&language=en-US`,
       },
     ];
   },
---
+++ pages/movies/[id].js
@@ -0,0 +1,6 @@
+import { useRouter } from "next/router";
+
+export default function Detail() {
+  const router = useRouter();
+  return <div>{router.query.title || "Loading..."}</div>;
+}
